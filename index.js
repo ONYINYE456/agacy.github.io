a1965460e05acec7913f8f3a0023e29efaf32ffc
udeagbala onyinye sonia <soniaonyii@yahoo.com>
2023-12-25 23:30:45 +0100

    Add files via upload
    push

---
+++ index.js
@@ -0,0 +1,19 @@
+function signUp() {
+    
+    window.location.href = 'welcome.html';  
+}
+
+function showLogin() {
+    window.location.href = 'login.html';  
+}
+
+function showsignup() {
+    window.location.href = 'welcome.html';  
+}
+
+
+
+
+
+
+
